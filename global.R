f8f65f64138eb6d4259f48a1a63f25540167d713
Andrew Dau <andrew_dau@hotmail.com>
2025-03-09 00:00:59 +0000

    updates to start using the database.  was able to connect to the submissions and the tiebreaker that is loaded to db.  still more work to be done...

--- global.R
+++ global.R
@@ -22,12 +22,14 @@ library(emayili)
 Sys.setenv(TZ='US/Pacific')
 
 raw_selections <- read.csv("./Data/raw_selections_real_test.csv")
-raw_selections_long <- raw_selections %>% select(Entry, Team1, Team2, Team3, Team4, Team5, Team6, Team7, Team8, Team9, Team10, Team11, Team12, Team13, Team14, Team15, Team16, Team17, Team18, Team19, Team20, Team21, Team22, Team23, Team24) %>%
-  pivot_longer(!Entry, names_to = "teamgroup", values_to = "Team") %>% filter(!is.na(Team))
+# raw_selections_long <- raw_selections %>% select(Entry, Team1, Team2, Team3, Team4, Team5, Team6, Team7, Team8, Team9, Team10, Team11, Team12, Team13, Team14, Team15, Team16, Team17, Team18, Team19, Team20, Team21, Team22, Team23, Team24) %>%
+#   pivot_longer(!Entry, names_to = "teamgroup", values_to = "Team") %>% filter(!is.na(Team))
+
+
 #standings <- read.csv("./Data/scores_temp.csv")
 #standings <- standings %>% arrange(desc(Score))
 master <- read.csv("./Data/all_teams.csv", stringsAsFactors = FALSE)  
-tiebreaker <- read.csv("./Data/tiebreaker_test.csv", stringsAsFactors = FALSE)  
+tiebreaker2 <- read.csv("./Data/tiebreaker_test.csv", stringsAsFactors = FALSE)  
 #tiebreaker <- read.csv("./Data/raw_selections.csv") %>% mutate(Entry = Your.Name.) %>% mutate(TieBreaker = as.integer(Total.Points.In.The.National.Title.Game..used.as.tiebreaker..)) %>% select(Entry, TieBreaker)
 #tiebreaker <- read.csv("./Data/tiebreaker_master.csv", stringsAsFactors=FALSE)
 
@@ -45,5 +47,18 @@ con <- dbConnect(
   password = Sys.getenv('password')
 )
 
+
+
 # Get teams data from database
 teams <- dbGetQuery(con, "SELECT team_name, seed, cost FROM march_madness_teams") %>% arrange(seed)
+
+raw_selections_long <- dbGetQuery(con, "SELECT entry_name as Entry, team_name as Team  FROM submissions") %>%
+  rename_with(~ paste0(toupper(substr(., 1, 1)), substr(., 2, nchar(.))))
+
+ tiebreaker <- dbGetQuery(con, "SELECT entry_name as Entry, tiebreaker_points as Tiebreaker  FROM submission_totals") %>%
+   rename_with(~ paste0(toupper(substr(., 1, 1)), substr(., 2, nchar(.)))) %>%
+   dplyr::rename(TieBreaker = Tiebreaker) %>%
+   mutate (X = row_number()) %>% 
+   select(X, Entry, TieBreaker)
+#so weird that this was breaking without row index.  Probably need to fix that.
+ 
